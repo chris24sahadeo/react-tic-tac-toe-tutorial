dacdba0f9cd43af4baf15de6ce7f39ab7cbe7ea6
Christopher Sahadeo <chris@virtanatech.com>
2023-09-07 19:41:40 -0400

    Completed the tutorial, but winner logic broken.
    Also trying to figure out how to debug in VSCode through Docker.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ tic-tac-toe-app-1  |   Line 2:  'React' must be in scope when using JSX  react/r
 */
 import React, {useState} from 'react';
 
-function Square() {
+function Square({value, onSquareClick}) {
 
-  const [value, setValue] = useState(null);
+  // const [value, setValue] = useState(null);
 
-  function handleClick() {
-    setValue('X');
-  }
+  // function handleClick() {
+  //   setValue('X');
+  // }
 
   return (
     <button
       className="square"
-      onClick={handleClick}
+      onClick={onSquareClick}
     >
       {value}
     </button>
@@ -24,22 +24,64 @@ function Square() {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [winner, setWinner] = useState(null); // ['X', 'O'
+  const [status, setStatus] = useState('Next player: ' + (xIsNext ? 'X' : 'O'));
+
+  function calculateWinner() {
+    const lines = [
+      [0, 1, 2], // top row
+      [3, 4, 5], // middle row
+      [6, 7, 8], // bottom row
+      [0, 3, 6], // left column
+      [1, 4, 7], // middle column
+      [2, 5, 8], // right column
+      [0, 4, 8], // diagonal top left to bottom right
+      [2, 4, 6], // diagonal top right to bottom left
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [j, k, l] = lines[i];
+      if (squares[j] && squares[j] === squares[k] && squares[j] === squares[l]) {
+        setWinner(squares[j]);
+      }
+    }
+  }
+
+  function handleClick(i) {
+    if (squares[i] || winner) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+
+    calculateWinner();
+    if (winner) {
+      setStatus('Winner: ' + winner);
+    } else {
+      setStatus('Next player: ' + (xIsNext ? 'X' : 'O'));
+    }
+  }
+
   return (
     <React.Fragment>
+      <div className="status">{status}</div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
       </div>
     </React.Fragment>
   );
